4563c0d554a0794d818803c027b89a5bb582e7b4
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2022-11-30 09:55:31 -0800

    Finished blocking movement
    the world now has borders

--- RubysAdventureJonasson/Assets/Scripts/RubyController.cs
+++ RubysAdventureJonasson/Assets/Scripts/RubyController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,6 +29,7 @@ public class RubyController : MonoBehaviour
         position.x = position.x + 3.0f * Horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         //setting the new position
-        transform.position = position; 
+
+        rigidbody2d.MovePosition(position);
     }
 }
