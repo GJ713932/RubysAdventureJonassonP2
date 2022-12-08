fc25ccd91b6b5d66db342425aa2e57bbaa6ac02d
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2022-12-08 10:04:32 -0800

    started sprite animation
    started to animate the bot

--- RubysAdventureJonasson/Assets/Scripts/EnemyController.cs
+++ RubysAdventureJonasson/Assets/Scripts/EnemyController.cs
@@ -5,19 +5,59 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
 
-
-    Rigidbody2D rigidbody2d;
-
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
 
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
-    
     void Update()
     {
+        timer -= Time.deltaTime;
+
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+
+
+        if (vertical)
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+
+
+        rigidbody2D.MovePosition(position);
+
+
         
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
