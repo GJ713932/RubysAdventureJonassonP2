274aa67950829ac7a8452ffa60fa8fe7941c3545
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2022-12-12 10:09:35 -0800

    finished projectiles

--- RubysAdventureJonasson/Assets/Scripts/Projectile.cs
+++ RubysAdventureJonasson/Assets/Scripts/Projectile.cs
@@ -8,7 +8,7 @@ public class Projectile : MonoBehaviour
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
@@ -16,7 +16,10 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Launch(Vector2 direction, float force)
@@ -26,6 +29,14 @@ public class Projectile : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+
+        Destroy(gameObject);
+
         //we also add a debug log to know what the projectile touch
         Debug.Log("Projectile Collision with " + other.gameObject);
         Destroy(gameObject);
